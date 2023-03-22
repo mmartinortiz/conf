134e3be395d8f21d987fbe4db1cc977e1db0f891
manolo <mmartinortiz@gmail.com>
2023-03-22 20:57:11 +0100

    Basic setup

---
+++ README.md
@@ -0,0 +1,6 @@
+# Personal configuration
+
+```bash
+ansible-playbook -i hosts -e "user_name=newuser" playbook.yaml
+```
+
---
+++ config.fish
@@ -0,0 +1,26 @@
+set -g theme_powerline_fonts yes
+set -g theme_color_scheme solarized-dark
+set -g theme_nerd_fonts yes
+set -g theme_display_date no
+set -g theme_nerd_fonts yes
+
+fish_add_path ~/.local/bin
+
+# Install Solarized colors
+if not test -d $HOME/Projects/dircolors-solarized
+  mkdir -p $HOME/Projects
+  git clone https://github.com/seebi/dircolors-solarized $HOME/Projects/dircolors-solarized
+end
+
+eval (dircolors -c $HOME/Projects/dircolors-solarized/dircolors.ansi-universal)
+
+# Replace 'cat' with 'batchat'
+if which batcat > /dev/null;
+  alias cat "batcat"
+end
+
+# Enable VI key bindings
+if test -n "$TERM"
+  fish_vi_key_bindings
+end
+
