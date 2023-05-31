28ce8cb7cec263022df41d84e357cb9e16ff467f
manolo <mmartinortiz@gmail.com>
2023-05-31 11:38:52 +0200

    Set default editors. Keep old cat

--- config.fish
+++ config.fish
@@ -4,6 +4,10 @@ set -g theme_nerd_fonts yes
 set -g theme_display_date no
 set -g theme_nerd_fonts yes
 
+set -Ux EDITOR $(which vim)
+set -Ux SUDO_EDITOR $(which vim)
+set -Ux SYSTEM_EDITOR $(which vim)
+
 fish_add_path ~/.local/bin
 
 # Install Solarized colors
@@ -16,6 +20,7 @@ eval (dircolors -c $HOME/Projects/dircolors-solarized/dircolors.ansi-universal)
 
 # Replace 'cat' with 'batchat'
 if which batcat > /dev/null;
+  alias oldcat $(which cat)
   alias cat "batcat"
 end
 
